c7fe2d6cea37e42e2723aa9daa16a41d38504cf3
FranciscoErberto <31253640+FranciscoErberto@users.noreply.github.com>
2018-05-01 21:26:43 -0300

    Update file.c

--- lzw/src/file.c
+++ lzw/src/file.c
@@ -1,10 +1,12 @@
+/* Para que tenhamos um byte com 8 bits é necessário dividir os 12 bits que é recebido na entrada
+assim gravando os 8 bits em um byte na saída e os 4 bits restantes são salvos na próxima vez (interação)*/
 
 #include <stdio.h>
 
 int cod_res = 0; 
 int cod_resBits;
 
-void wrBinario(FILE * saida, int cod){
+void wrBits(FILE * saida, int cod){
 
   if(cod_res > 0){
     int ant_Cod = (cod_resBits == 4)+(cod == 8);
@@ -22,4 +24,24 @@ void wrBinario(FILE * saida, int cod){
     
   }
   
+}
+
+int rdBits (FILE * entrada){
+
+  int cod = fgetc(entrada);
+  if(cod == EOF){
+    return 0;
+  }
+  if(cod_res > 0){
+    cod = (cod_resBits == 8) + cod;
+    cod_res = 0;
+  }else{
+    int prox_Cod = fgetc(entrada);
+    cod_resBits = prox_Cod & 0xF;
+    cod_res = 1;
+    
+    cod = (cod == 4)+(prox_Cod == 4);
+    
+  }
+  return cod;
 }
