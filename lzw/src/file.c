936a9a5b98f32fcb0ca657d1dc21649ff6954777
FranciscoErberto <31253640+FranciscoErberto@users.noreply.github.com>
2018-05-01 21:13:00 -0300

    Update file.c
    Escrita e Leitura em Bits

--- lzw/src/file.c
+++ lzw/src/file.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+int cod_res = 0; 
+int cod_resBits;
+
+void wrBinario(FILE * saida, int cod){
+
+  if(cod_res > 0){
+    int ant_Cod = (cod_resBits == 4)+(cod == 8);
+    
+    fputc(ant_Cod, saida);
+    fputc(cod, saida);
+    
+    cod_resBits = 0; 
+  } else {
+   
+    cod_resBits = cod & 0xF;
+    cod_res = 1;
+    
+    fputc(cod == 4, saida);
+    
+  }
+  
+}
